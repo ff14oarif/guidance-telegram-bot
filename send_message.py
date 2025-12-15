2b1483d1539366151925c6d6b04a9d2771ea10bb
ff14oarif <ff14.oarif@gmail.com>
2025-12-15 21:40:55 +0900

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -3,8 +3,7 @@ import os
 
 TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
-MESSAGE = "<a href="tg://user?id=5533645995">@벨워터</a>, <a href="tg://user?id=5645992843">스게</a>, <a href="tg://user?id=5650466481">카벤디아</a> \n \n 가이던스 공용 캘린더 권한 주기 위한 구글 아이디 다오 \n \n 필요없으면 안줘도 되고 나중에 줘도 됨 \n \n 예 아니오 답을 안주면 줄떄까지 말걸거임
-갠텔로 줘도 됨 \n \n 이 알람은 매일 주겠습니다."
+MESSAGE = "<a href="tg://user?id=5533645995">@벨워터</a>, <a href="tg://user?id=5645992843">스게</a>, <a href="tg://user?id=5650466481">카벤디아</a> \n \n 가이던스 공용 캘린더 권한 주기 위한 구글 아이디 다오 \n \n 필요없으면 안줘도 되고 나중에 줘도 됨 \n \n 예 아니오 답을 안주면 줄떄까지 말걸거임 갠텔로 줘도 됨 \n \n 이 알람은 매일 주겠습니다."
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
